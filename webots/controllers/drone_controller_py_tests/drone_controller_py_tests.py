c56b9743f44a1dd133d07b1d5c92005ca3bf3d95
losca <loscarciglia@gmail.com>
2023-05-15 15:57:59 +0200

    IBVS_GT test. no saving data

--- webots/controllers/drone_controller_py_tests/drone_controller_py_tests.py
+++ webots/controllers/drone_controller_py_tests/drone_controller_py_tests.py
@@ -3,7 +3,7 @@
 #  | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
 #  | / ,..´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
 #     +.......   /_____/_/\__/\___/_/   \__,_/ /___/\___/
-
+ 
 # MIT License
 
 # Copyright (c) 2022 Bitcraze
@@ -14,7 +14,8 @@
 """crazyflie_controller_py controller."""
 
 
-from controller import Robot
+# from controller import Robot
+from controller import Supervisor
 from controller import Motor
 from controller import InertialUnit
 from controller import GPS
@@ -23,21 +24,35 @@ from controller import Keyboard
 from controller import Camera
 from controller import DistanceSensor
 
-from math import cos, sin
-
+import os, datetime
+import cv2
+import csv, json
 import numpy as np
+import random
+import errno
+import itertools
+import transforms3d
+import machinevisiontoolbox as mvtb
+from spatialmath import SE3
+random.seed()
+
+from math import cos, sin, degrees, radians
+from ai import cs
 
 import sys
-sys.path.append('../../../controllers')
-# import os
-# print(os.listdir('../../../controllers'))
+sys.path.append('../../../controllers/')
+
+# Import the path for the corner detection module
+sys.path.append('../../../../scarciglia-nanodrone-gate-detection/')
+
+from src import corner, rotation, geometry
 from pid_controller import pid_velocity_fixed_height_controller
 
 FLYING_ATTITUDE = 1
 
 if __name__ == '__main__':
 
-    robot = Robot()
+    robot = Supervisor()
     timestep = int(robot.getBasicTimeStep())
     
     sampling_frequency = timestep
@@ -74,15 +89,46 @@ if __name__ == '__main__':
     range_right = robot.getDevice("range_right")
     range_right.enable(timestep)
 
+    # Crazyflie
+    crazyflie_node = robot.getFromDef("CRAZYFLIE")
+    translation_drone = crazyflie_node.getField('translation')
+    rotation_drone = crazyflie_node.getField('rotation')
+    camera_node = crazyflie_node.getField('children').getMFNode(1)
+    camera_drone_tr = camera_node.getField('translation').getSFVec3f()
+
+    # Gate
+    gate_node = robot.getFromDef("GATE")
+    translation_gate = gate_node.getField('translation').getSFVec3f()
+    gate_center = gate_node.getField('children').getMFNode(4).getField('translation').getSFVec3f()
+    gate_rot = gate_node.getField('rotation').getSFRotation()
+
+    # Gate pose. This is used to transform the gate points.
+    T_G = SE3(translation_gate)*SE3(transforms3d.axangles.axangle2aff(gate_rot[:-1], gate_rot[-1]))
+
+    br = gate_node.getField('children').getMFNode(0).getField('translation').getSFVec3f()
+    bl = gate_node.getField('children').getMFNode(1).getField('translation').getSFVec3f()
+    tl = gate_node.getField('children').getMFNode(2).getField('translation').getSFVec3f()
+    tr = gate_node.getField('children').getMFNode(3).getField('translation').getSFVec3f()
+
+    # Rotate gate points. Transform in homogenous coordinates, perform the matmul, and then pick the first three entries.
+    tl = (np.array(T_G)@np.append(tl, 1))[:-1]
+    bl = (np.array(T_G)@np.append(bl, 1))[:-1]
+    br = (np.array(T_G)@np.append(br, 1))[:-1]
+    tr = (np.array(T_G)@np.append(tr, 1))[:-1]
+
+    P = np.array([
+        tl, bl, br, tr
+    ]).T
+    print('Gate data (br, bl, tl, tr, gate_center, translation_gate):', br, bl, tl, tr, gate_center, translation_gate)
+
     ## Get keyboard
     keyboard = Keyboard()
     keyboard.enable(timestep)
 
     ## Initialize variables
 
-    past_x_global = 0
-    past_y_global = 0
-    past_z_global = 0
+    past_x_global, past_y_global = (None,None)
+    past_z_global = None
     past_time = robot.getTime()
 
     # Crazyflie velocity PID controller
@@ -107,31 +153,73 @@ if __name__ == '__main__':
     print("- Use the up, back, right and left button to move in the horizontal plane\n");
     print("- Use Q and E to rotate around yaw ");
     print("- Use W and S to go up and down\n ")
-
-    # Capture n steps
-    step = 0
+       
+    ########### ------------------ HOVERING ------------------ ###########
 
     # Hovering
     prev_step = False
     hovering_steps = 0 # Counter used to check how many times we are in the desired state.
     max_hovering_steps = 500
 
+    ########### ------------------ HOVERING ------------------ ###########
+    
+    ########### ------------------ TASKS ------------------ ###########
     
     # Desired states
     tasks = {}
 
     take_off = True
-    take_off_info = {'setpoints': {'velocity.x':0.0, 'velocity.y':0.0, 'position.z':1, 'attitudeRate.yaw':0.0}}
+    take_off_info = {'setpoints': {'velocity.x':0.0, 'velocity.y':0.0, 'position.z':1, 'attitudeRate.yaw':0.0}, 'num_steps':1000}
     tasks[0] = take_off_info
 
-    first_task = False
-    first_task_info = {'setpoints': {'velocity.x':0.1, 'velocity.y':.1, 'velocity.z':.1, 'attitudeRate.yaw':0}, 'num_steps':1000}
-    first_task_step = 0
-    tasks[1] = first_task_info
+    visual_servoing = False
+    tasks[1] = {'visual_servoing':True}
+    
+    cross_the_gate = False
+    num_seconds = 10
+    cross_the_gate_info = {'setpoints': {'velocity.x':0.1, 'velocity.y':0.0, 'position.z':None, 'attitudeRate.yaw':0.0}, 'num_steps':num_seconds*sampling_frequency}
+    cross_the_gate_steps = 0
+    tasks[2] = cross_the_gate_info
 
-    second_task = False
+    landing = False
+    landing_info = {'setpoints': {'velocity.x':0.0, 'velocity.y':0.0, 'position.z':0.0, 'attitudeRate.yaw':0.0}, 'num_steps':1000}
+    tasks[3] = landing_info
 
     dataset = {'info':tasks, 'sampling_frequency':sampling_frequency, 'max_hovering_steps':max_hovering_steps}
+    
+    ########### ------------------ TASKS ------------------ ###########
+
+    # OpenCV show images
+    cv2.startWindowThread()
+    cv2.namedWindow("preview")
+    colors = ['r', 'b', 'g', 'y']
+    
+    ########### ------------------ VISUAL SERVOING ------------------ ###########
+    
+    f = 0.0006
+    pixel_size = (3.6e-6, 3.6e-6)
+    img_size = (320,320)
+    cam = mvtb.CentralCamera(rho=pixel_size[0], imagesize=img_size, f=f)
+
+    # Depth value
+    Z = 0.34
+
+    # Desired positions
+    wide = 200 # square 400px wide in the center of the camera frame
+    pd = np.array([[cam.pp[0] - wide/2, cam.pp[1] - wide/2], # TL is the 1st
+                [cam.pp[0] - wide/2, cam.pp[1] + wide/2], # BL is the 2nd
+                [cam.pp[0] + wide/2, cam.pp[1] + wide/2], # BR is the 3rd
+                [cam.pp[0] + wide/2, cam.pp[1] - wide/2]])# TR is the 4th
+    pd = pd.T # set the x and y coordinates by column
+
+    print(f'[POINTS DESIRED]\n{pd}')
+
+    lmda = 0.08
+
+    thresh = 5e-1
+    err = np.inf
+
+    ########### ------------------ VISUAL SERVOING ------------------ ###########
 
     height_desired = take_off_info['setpoints']['position.z']
 
@@ -142,7 +230,7 @@ if __name__ == '__main__':
     yaw_desired = 0
     height_diff_desired = 0
     
-    it_idx = 0
+    it_idx = 0 # Iteration index
 
     # Main loop:
     while robot.step(timestep) != -1:
@@ -165,7 +253,6 @@ if __name__ == '__main__':
             v_x_global = (x_global - x_global)/dt
             v_y_global = (y_global - y_global)/dt
             v_z_global = (z_global - z_global)/dt
-            it_idx += 1
         
         else:
             v_x_global = (x_global - past_x_global)/dt
@@ -188,7 +275,14 @@ if __name__ == '__main__':
         v_x = v_x_global * cosyaw + v_y_global * sinyaw
         v_y = - v_x_global * sinyaw + v_y_global * cosyaw
 
+        # CAMERA IMAGES
+        w, h = camera.getWidth(), camera.getHeight()
+        cameraData = camera.getImage()  # Note: uint8 string
+        image = np.frombuffer(cameraData, np.uint8).reshape(h, w, 4) # BGR, alpha (transparency)
+
         if take_off:
+            
+            print("Take off!")
             
             info = tasks[0] # take_off_info
             
@@ -203,7 +297,7 @@ if __name__ == '__main__':
                     prev_step = isclose
                     if hovering_steps >= 500:
                         take_off = False
-                        first_task = True
+                        visual_servoing = True
                         print("Passing to the next task...")
                         # break
             else:
@@ -221,28 +315,66 @@ if __name__ == '__main__':
             # New height. Integrate v_z to get the next position.
             height_desired += height_diff_desired * dt 
 
-            ## Example how to get sensor data
-            ## range_front_value = range_front.getValue();
-            ## cameraData = camera.getImage()
-
-
             ## PID velocity controller with fixed height. Height given as position.
             motor_power = PID_CF.pid(dt, forward_desired, sideways_desired,
                                     yaw_desired, height_desired,
                                     roll, pitch, yaw_rate,
                                     altitude, v_x, v_y, gains)
         
-        elif first_task:
+        elif visual_servoing:
+            
+            print("Going in front of the gate...")
             
             info = tasks[1]
             
             print(info)
             
-            # Setpoints fashion (only in velocity)
-            forward_desired = info['setpoints']['velocity.x']
-            sideways_desired = info['setpoints']['velocity.y']
-            height_diff_desired = info['setpoints']['velocity.z']
-            yaw_desired = info['setpoints']['attitudeRate.yaw']
+            ########### ------------------ ROTATIONS ------------------ ###########
+            rotation_matrix_world_drone = rotation.rotation_matrix(roll, pitch, yaw)
+            wd_tr = np.array([x_global, y_global, z_global])
+            extrinsic_matrix_world_drone = rotation.get_extrinsic_matrix(rotation_matrix_world_drone, translation_vector=wd_tr)
+            
+            rotation_matrix_drone_camera = rotation.rotation_matrix(roll=-np.pi/2, pitch=0, yaw=-np.pi/2)
+            dc_tr = camera_drone_tr
+            extrinsic_matrix_drone_camera = rotation.get_extrinsic_matrix(rotation_matrix_drone_camera, translation_vector=dc_tr)
+
+            extrinsic_matrix = extrinsic_matrix_world_drone@extrinsic_matrix_drone_camera
+            ########### ------------------ ROTATIONS ------------------ ###########
+            
+            # print(roll, pitch, yaw)
+            
+            T_C = SE3(wd_tr)*SE3.RPY(roll,pitch,yaw)*SE3(dc_tr)*SE3.RPY(-np.pi/2, 0, -np.pi/2)
+            # print(T_C)
+            # print(wd_tr)
+            p_detected = cam.project_point(P, pose=SE3(T_C, check=False)) 
+            # print(p_detected)
+            ########### ------------------ VISUAL SERVOING ------------------ ###########
+            # image-plane error
+            try:
+                e = pd - p_detected
+                err = np.linalg.norm(e)
+                if err <= thresh:
+                    visual_servoing = False
+                    cross_the_gate = True
+                    print("Passing to the next task...")
+            except:
+                continue
+            
+            # stacked image Jacobian
+            J = cam.visjac_p(p_detected, Z)
+            v_camera = lmda * np.linalg.pinv(J) @ e.T.flatten()
+
+            # Twist velocity from camera frame to drone frame
+            twist_drone_camera = geometry.velocity_twist_matrix(rotation_matrix_drone_camera, dc_tr)
+            v_drone = twist_drone_camera@v_camera
+            ibvs_v_x, ibvs_v_y, ibvs_v_z, ibvs_w_x, ibvs_w_y, ibvs_w_z = v_drone
+            
+            forward_desired = ibvs_v_x
+            sideways_desired = ibvs_v_y
+            yaw_desired = ibvs_w_z
+            height_diff_desired = ibvs_v_z
+
+            ########### ------------------ VISUAL SERVOING ------------------ ###########
             
             # New height. Integrate v_z to get the next position.
             height_desired += height_diff_desired * dt 
@@ -253,17 +385,84 @@ if __name__ == '__main__':
                                     roll, pitch, yaw_rate,
                                     altitude, v_x, v_y, gains)
             
-            first_task_step += 1
-        
-            if first_task_step == info['num_steps']:
-                first_task = False
-                print("Passing to the next task...")
-        
-        else:
+            # Show image
+            for id, col in enumerate(colors):
+                tl = pd[:,0]
+                bl = pd[:,1]
+                br = pd[:,2]
+                tr = pd[:,-1]
+                x, y = pd[:,id]
+                image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=1)
+                image = cv2.line(image, (int(tl[0]), int(tl[1])), (int(tr[0]), int(tr[1])), color=(255, 255, 255), thickness=1) # top-left, top-right
+                image = cv2.line(image, (int(tr[0]), int(tr[1])), (int(br[0]), int(br[1])), color=(255, 255, 255), thickness=1) # top-right, bottom-right
+                image = cv2.line(image, (int(br[0]), int(br[1])), (int(bl[0]), int(bl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-right
+                image = cv2.line(image, (int(bl[0]), int(bl[1])), (int(tl[0]), int(tl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-left
+                x, y = p_detected[:,id]
+                image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=-1)
 
+            cv2.imshow("preview", cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+            cv2.waitKey(timestep)
+            
+        elif cross_the_gate:
+            
+            print("Crossing the gate...")
+            
+            info = tasks[2]
+            
+            print(info)
+            
+            if cross_the_gate_steps == info['num_samples']:
+                
+                hovering_steps += 1
+                
+                if hovering_steps >= 500:
+                    cross_the_gate = False
+                    landing = True
+                    print("Passing to the next task...")
+            else:
+                hovering_steps = 0
+        
+            
+            # Setpoints fashion (only in velocity)
+            forward_desired = info['setpoints']['velocity.x']
+            sideways_desired = info['setpoints']['velocity.y']
+            height_desired = info['setpoints']['position.z']
+            yaw_desired = info['setpoints']['attitudeRate.yaw']
+            
+            # New height. Integrate v_z to get the next position.
+            height_desired += height_diff_desired * dt 
+
+            ## PID velocity controller with fixed height. Height given as position.
+            motor_power = PID_CF.pid(dt, forward_desired, sideways_desired,
+                                    yaw_desired, height_desired,
+                                    roll, pitch, yaw_rate,
+                                    altitude, v_x, v_y, gains)
+        elif landing:
+            
+            print("Landing...")
+            
+            info = tasks[2]
+            
+            print(info)
+            
+            # Setpoints fashion (only in velocity)
+            forward_desired = info['setpoints']['velocity.x']
+            sideways_desired = info['setpoints']['velocity.y']
+            height_desired = info['setpoints']['position.z']
+            yaw_desired = info['setpoints']['attitudeRate.yaw']
+            
+            # New height. Integrate v_z to get the next position.
+            height_desired += height_diff_desired * dt 
+
+            ## PID velocity controller with fixed height. Height given as position.
+            motor_power = PID_CF.pid(dt, forward_desired, sideways_desired,
+                                    yaw_desired, height_desired,
+                                    roll, pitch, yaw_rate,
+                                    altitude, v_x, v_y, gains)
+        else:
+            print("Landed!")    
             break # No more tasks
 
-        # print(motor_power)
         
         m1_motor.setVelocity(-motor_power[0])
         m2_motor.setVelocity(motor_power[1])
@@ -315,11 +514,11 @@ if __name__ == '__main__':
                           'm4':motor_power[3],}
         
         # Save data
-        dataset[step] = data
+        dataset[it_idx] = data
 
         ########### ------------------ SAVING THINGS -------------------- ###########
         
-        step += 1
+        it_idx += 1
         
     import pickle, os
 
