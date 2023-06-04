b79fa1e3ca996c74d3c137ec71e7f10ca7a7289d
Lorenzo Scarciglia <loscarciglia@gmail.com>
2023-06-04 23:16:18 +0200

    +-30° noisy tests

--- webots/controllers/drone_controller_py_tests/drone_controller_py_tests.py
+++ webots/controllers/drone_controller_py_tests/drone_controller_py_tests.py
@@ -1,19 +1,3 @@
-#  ...........       ____  _ __
-#  |  ,-^-,  |      / __ )(_) /_______________ _____  ___
-#  | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-#  | / ,..´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#     +.......   /_____/_/\__/\___/_/   \__,_/ /___/\___/
- 
-# MIT License
-
-# Copyright (c) 2022 Bitcraze
-
-# @file crazyflie_controllers_py.py
-# Controls the crazyflie motors in webots in Python
-
-"""crazyflie_controller_py controller."""
-
-
 # from controller import Robot
 from controller import Supervisor
 from controller import Motor
@@ -50,6 +34,16 @@ from pid_controller import pid_velocity_fixed_height_controller
 
 FLYING_ATTITUDE = 1
 np.random.seed(0)
+
+########### ------------------ TEST PARAMETERS -------------------- ###########
+relative_angle = np.deg2rad(+30)
+test = {# 'orientation':(0,0,np.pi/2), # Perpendicular to the gate
+        'orientation':(0,0,np.pi/2 + relative_angle), # Facing the gate centre
+        'theta':relative_angle, 
+        'take_off_height':1.5, 
+        'sigma':0.289}
+########### ------------------ TEST PARAMETERS -------------------- ###########
+
 ########### ------------------ SAVING THINGS -------------------- ###########
     
 # Set to True if you want to collect data
@@ -58,7 +52,8 @@ collect_data = True
 if collect_data:
         
     parent_folder = '../../datasets/EXP-5-IBVS'
-    folder = parent_folder +'/tests/'+ '01_corner_det_test'
+    folder = parent_folder +'/tests_noisy_GT/'+ '60_test'
+
 
     imgs_folder = f'{folder}/imgs/'
     imgs_ibvs_folder = f'{folder}/imgs_ibvs/'
@@ -167,9 +162,25 @@ if __name__ == '__main__':
     ]).T
     print('Gate data (br, bl, tl, tr, gate_center, translation_gate):', br, bl, tl, tr, gate_center, translation_gate)
 
-    ## Get keyboard
-    keyboard = Keyboard()
-    keyboard.enable(timestep)
+    ########### ------------------ DRONE STARTING POSITION ------------------ ###########
+    r = 2
+    theta = test['theta']
+
+    x = r*np.sin(theta)
+    y = - r*np.cos(theta)
+
+    starting_height = translation_drone.getSFVec3f()[-1]
+    translation_drone.setSFVec3f([x, y, starting_height])
+
+    # Orientation
+    roll, pitch, yaw = test['orientation']
+    rot = transforms3d.euler.euler2axangle(roll, -pitch, yaw)
+    ax_angle = list(rot[0])
+    ax_angle.append(rot[-1])    
+
+    crazyflie_node.getField('rotation').setSFRotation(ax_angle)
+    ########### ------------------ DRONE STARTING POSITION ------------------ ###########
+
 
     ## Initialize variables
 
@@ -204,7 +215,7 @@ if __name__ == '__main__':
     tasks['order'] = ['take_off', 'visual_servoing', 'cross_the_gate', 'land']
 
     take_off = True
-    take_off_info = {'setpoints': {'velocity.x':0.0, 'velocity.y':0.0, 'position.z':1, 'attitudeRate.yaw':0.0}}
+    take_off_info = {'setpoints': {'velocity.x':0.0, 'velocity.y':0.0, 'position.z':test['take_off_height'], 'attitudeRate.yaw':0.0}}
     tasks['take_off'] = take_off_info
 
     visual_servoing = False
@@ -213,7 +224,8 @@ if __name__ == '__main__':
     GT_detection = np.zeros(shape=(3,2,4))
     detections = []
     GT_detections = []
-    filter = {'alpha':0.5, 'order':1}
+    GT_jitter_detections = []
+    filter = {'alpha':1, 'order':1}
     vs_counter = 0
     track_error = False
     offset = None
@@ -280,7 +292,8 @@ if __name__ == '__main__':
                         'rotation': gate_node.getField('rotation').getSFRotation()}
     dataset['camera'] = {'f':f, 'pixel_size':pixel_size, 'img_size':img_size }
     dataset['ibvs'] = {'lambda': lmda, 'threshold': thresh, 'Z':{'estimated':False, 'value':Z}}
-    
+    dataset['test'] = test
+
     ########### ------------------ SAVING THINGS ------------------ ###########
 
     height_desired = take_off_info['setpoints']['position.z']
@@ -423,12 +436,65 @@ if __name__ == '__main__':
                 GT_p_detected = corner.weigh_detection(GT_detection, order=filter['order'], alpha=filter['alpha'])
                        
             GT_detections.append(GT_p_detected)
+                        
+            jitter = np.random.normal(scale=test['sigma'], size=8).reshape(2,4) 
+            
+            GT_jitter_p_detected = GT_p_detected + jitter
+            
+            GT_jitter_detections.append(GT_jitter_p_detected)
             
             # image-plane error
             try:
                 
-                GT_e = pd - GT_p_detected
+                GT_e = pd - GT_jitter_p_detected
                 GT_err = np.linalg.norm(GT_e)
+                
+                print(f"Error: {GT_err:.2f}")
+
+                if GT_err <= 50 and track_error is False:
+                    track_error = True
+                    offset = it_idx
+                    print("Collect errors...")
+                
+                if track_error and median_err > 50:
+                    idx = it_idx - offset
+                    if idx >= len(errors):
+                        idx = len(errors)
+                    print(f"Collecting errors: {idx}/{errors.shape[-1]}")                    
+                    if idx == len(errors):
+                        median_err = np.median(errors)
+                        print("Median error:", median_err)
+                        if median_err <= 50:
+                            visual_servoing = False
+                            cross_the_gate = True
+                            
+                            # Save the current altitude in order to pass the gate
+                            # tasks['cross_the_gate']['setpoints']['position.z'] = z_global 
+                            # Now set as velocity.z = 0.0
+
+                            info['ending_step'] = it_idx
+                            
+                            print("Crossing the gate...")
+                        else:
+                            # Shift errors
+                            temp = errors[1:]
+                            errors[:-1] = temp
+                            errors[-1] = GT_err
+                    else:
+                        errors[idx] = GT_err
+
+                if GT_err <= thresh:
+                    
+                    visual_servoing = False
+                    cross_the_gate = True
+                    
+                    # Save the current altitude in order to pass the gate
+                    # tasks['cross_the_gate']['setpoints']['position.z'] = z_global 
+                    # Now set as velocity.z = 0.0
+
+                    info['ending_step'] = it_idx
+                    
+                    print("Crossing the gate...")
             
             except Exception as e:
                 
@@ -438,7 +504,7 @@ if __name__ == '__main__':
             
 
             # stacked image Jacobian
-            J = cam.visjac_p(GT_p_detected, Z)
+            J = cam.visjac_p(GT_jitter_p_detected, Z)
             v_camera = lmda * np.linalg.pinv(J) @ GT_e.T.flatten()
    
             # Twist velocity from camera frame to drone frame
@@ -446,8 +512,39 @@ if __name__ == '__main__':
             
             v_drone = twist_drone_camera@v_camera
             GT_ibvs_v_x, GT_ibvs_v_y, GT_ibvs_v_z, GT_ibvs_w_x, GT_ibvs_w_y, GT_ibvs_w_z = v_drone
+            
+            forward_desired = GT_ibvs_v_x
+            sideways_desired = GT_ibvs_v_y
+            yaw_desired = GT_ibvs_w_z
+            height_diff_desired = GT_ibvs_v_z
+            
+            # New height. Integrate v_z to get the next position.
+            height_desired += height_diff_desired * dt 
 
             ########### ------------------ GT VISUAL SERVOING ------------------ ##########
+
+            # Show image
+            for id, col in enumerate(colors):
+                tl = pd[:,0] # 0
+                bl = pd[:,1] # 1
+                br = pd[:,2] # 2
+                tr = pd[:,3] # 3 
+                x, y = pd[:,id] # Desired
+                image = cv2.putText(image, text=str(id), org = (int(x),int(y)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255,255,255), thickness = 1)
+                image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=1)
+                image = cv2.line(image, (int(tl[0]), int(tl[1])), (int(tr[0]), int(tr[1])), color=(255, 255, 255), thickness=1) # top-left, top-right
+                image = cv2.line(image, (int(tr[0]), int(tr[1])), (int(br[0]), int(br[1])), color=(255, 255, 255), thickness=1) # top-right, bottom-right
+                image = cv2.line(image, (int(br[0]), int(br[1])), (int(bl[0]), int(bl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-right
+                image = cv2.line(image, (int(bl[0]), int(bl[1])), (int(tl[0]), int(tl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-left
+                x, y = GT_p_detected[:,id] # Detected
+                image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=-1)
+                image = cv2.putText(image, text=str(id), org = (int(x),int(y)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255,255,255), thickness = 1)
+                if collect_data:
+                    # Save the image
+                    cv2.imwrite(imgs_ibvs_folder+f'/img_{it_idx}.png', cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    
+            cv2.imshow("Drone Camera", cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+            cv2.waitKey(timestep)
             
             ########### ------------------ DETECTION VISUAL SERVOING ------------------ ###########
 
@@ -475,54 +572,6 @@ if __name__ == '__main__':
                 
                 e = pd - p_detected
                 err = np.linalg.norm(e)
-
-                print(f"Error: {err:.2f}")
-
-                if err <= 50 and track_error is False:
-                    track_error = True
-                    offset = it_idx
-                    print("Collect errors...")
-                
-                if track_error and median_err > 50:
-                    idx = it_idx - offset
-                    if idx >= len(errors):
-                        idx = len(errors)
-                    print(f"Collecting errors: {idx}/{errors.shape[-1]}")                    
-                    if idx == len(errors):
-                        # offset = it_idx
-                        median_err = np.median(errors)
-                        print("Median error:", median_err)
-                        if median_err <= 50:
-                            visual_servoing = False
-                            cross_the_gate = True
-                            
-                            # Save the current altitude in order to pass the gate
-                            # tasks['cross_the_gate']['setpoints']['position.z'] = z_global 
-                            # Now set as velocity.z = 0.0
-
-                            info['ending_step'] = it_idx
-                            
-                            print("Crossing the gate...")
-                        else:
-                            # Shift errors
-                            temp = errors[1:]
-                            errors[:-1] = temp
-                            errors[-1] = err
-                    else:
-                        errors[idx] = err
-
-                if err <= thresh:
-                    
-                    visual_servoing = False
-                    cross_the_gate = True
-                    
-                    # Save the current altitude in order to pass the gate
-                    # tasks['cross_the_gate']['setpoints']['position.z'] = z_global 
-                    # Now set as velocity.z = 0.0
-
-                    info['ending_step'] = it_idx
-                    
-                    print("Crossing the gate...")
                 
             except Exception as e:
                 
@@ -537,27 +586,6 @@ if __name__ == '__main__':
                 # Twist velocity from camera frame to drone frame
                 twist_drone_camera = geometry.velocity_twist_matrix(rotation_matrix_drone_camera, dc_tr)
                 v_drone = twist_drone_camera@v_camera
-
-                # Show image
-                for id, col in enumerate(colors):
-                    tl = pd[:,0] # 0
-                    bl = pd[:,1] # 1
-                    br = pd[:,2] # 2
-                    tr = pd[:,3] # 3 
-                    x, y = pd[:,id] # Desired
-                    image = cv2.putText(image, text=str(id), org = (int(x),int(y)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255,255,255), thickness = 1)
-                    image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=1)
-                    image = cv2.line(image, (int(tl[0]), int(tl[1])), (int(tr[0]), int(tr[1])), color=(255, 255, 255), thickness=1) # top-left, top-right
-                    image = cv2.line(image, (int(tr[0]), int(tr[1])), (int(br[0]), int(br[1])), color=(255, 255, 255), thickness=1) # top-right, bottom-right
-                    image = cv2.line(image, (int(br[0]), int(br[1])), (int(bl[0]), int(bl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-right
-                    image = cv2.line(image, (int(bl[0]), int(bl[1])), (int(tl[0]), int(tl[1])), color=(255, 255, 255), thickness=1) # bottom-left, top-left
-                    x, y = p_detected[:,id] # Detected
-                    image = cv2.circle(image, (int(x),int(y)), radius=2, color=(255, 255, 255), thickness=-1)
-                    image = cv2.putText(image, text=str(id), org = (int(x),int(y)), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255,255,255), thickness = 1)
-                    if collect_data:
-                        # Save the image
-                        cv2.imwrite(imgs_ibvs_folder+f'/img_{it_idx}.png', cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-        
             
             except Exception as e:
 
@@ -575,29 +603,19 @@ if __name__ == '__main__':
                 sample['target_points'] = pd
                 sample['detected_points'] = p_detected
                 sample['GT_detected_points'] = GT_p_detected
+                sample['GT_jitter_detected_points'] = GT_jitter_p_detected
                 sample['ibvs_error'] = err
-                sample['GT_ibvs_error'] = GT_err
+                sample['GT_jitter_ibvs_error'] = GT_err
                 data['IBVS'] = sample
             
                 ########### ------------------ SAVING THINGS -------------------- ###########
-
+                
                 break
                 
             ibvs_v_x, ibvs_v_y, ibvs_v_z, ibvs_w_x, ibvs_w_y, ibvs_w_z = v_drone
 
-            forward_desired = ibvs_v_x
-            sideways_desired = ibvs_v_y
-            yaw_desired = ibvs_w_z
-            height_diff_desired = ibvs_v_z
-            
-            # New height. Integrate v_z to get the next position.
-            height_desired += height_diff_desired * dt 
-
             ########### ------------------ DETECTION VISUAL SERVOING ------------------ ###########
             
-            cv2.imshow("Drone Camera", cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-            cv2.waitKey(timestep)
-
             ## PID velocity controller with fixed height. Height given as position.
             motor_power = PID_CF.pid(dt, forward_desired, sideways_desired,
                                     yaw_desired, height_desired,
@@ -612,8 +630,9 @@ if __name__ == '__main__':
             sample['target_points'] = pd
             sample['detected_points'] = p_detected
             sample['GT_detected_points'] = GT_p_detected
+            sample['GT_jitter_detected_points'] = GT_jitter_p_detected
             sample['ibvs_error'] = err
-            sample['GT_ibvs_error'] = GT_err
+            sample['GT_jitter_ibvs_error'] = GT_err
             data['IBVS'] = sample
             
             ########### ------------------ SAVING THINGS -------------------- ###########
@@ -750,7 +769,8 @@ if __name__ == '__main__':
         it_idx += 1
         
     dataset['info'] = tasks
-    dataset['detections'] = {'detection': detections, 'GT_detection': GT_detections,}
+    dataset['detections'] = {'detection': detections, 'GT_detection': GT_detections, 'GT_jitter_detection': GT_jitter_detections}
+    dataset['jitter'] = {'mean':0, 'std_dev':0.45}
 
     import pickle, os
 
